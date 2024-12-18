fdda253c88c1cc93914fc7a8edf9de0bdb106ad4
Daniele Santos <anninka20212gmai.com>
2024-12-18 20:48:53 -0300

    fundamentos

---
+++ fundamentos.py
@@ -0,0 +1,16 @@
+#primeiros comandos
+
+
+
+idade = 20
+
+peso = 56.4
+
+print("Daniele", idade, peso)
+
+'''toda variavél é um objeto'''
+
+nome = input ("Qual é o seu nome? ")
+idade = input ("Quantos anos você tem? ")
+peso = input ("Qual é o seu peso? ")
+print(nome, peso, idade)
