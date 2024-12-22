7b4d76d2136120e11feeca626128e4b1d05e5670
Daniele Santos <anninka20212gmai.com>
2024-12-22 15:42:10 -0300

    aritmeticos

---
+++ #07-aritmetico.py
@@ -0,0 +1,7 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+di = n1 // n2
+e = n1 ** n2
+print('O valor é \n {}, \n {}, \n {}, \n {}' .format(s, m, di, e))
